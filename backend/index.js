59b65e7960d8aceb655859836f7fe9c8aee181a7
hoppv <phamhopxd1@gmail.com>
2024-09-29 09:22:10 +0700

    database setup

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,21 @@
+import dotenv from "dotenv";
 import express from "express";
+import connectDB from "./db/connectDB.js";
+
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
 });
