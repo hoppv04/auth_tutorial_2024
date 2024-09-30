f747ab5d56e76112ad77a9ea9ed7d77a6ab7081d
hoppv <phamhopxd1@gmail.com>
2024-09-30 19:26:47 +0700

    check auth endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -216,3 +216,26 @@ export const resetPassword = async (req, res) => {
     });
   }
 };
+
+export const checkAuth = async (req, res) => {
+  try {
+    const user = await User.findById(req.userId).select("-password");
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        error: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    console.log("Error in checkAuth", error);
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./db/connectDB.js";
@@ -10,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
